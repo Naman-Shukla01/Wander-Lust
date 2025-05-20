b49ffa219dc786dbe8bda3a284a30e8285ea0664
Naman <Namanshukla819@gmail.com>
2025-05-20 21:50:57 +0530

    Error Resolved

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 const store = MongoStore.create({
-    mongourl: dbUrl,
+    mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
